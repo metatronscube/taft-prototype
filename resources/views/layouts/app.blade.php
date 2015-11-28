885e525438926e183d7ed64bdee3cd69ee663af7
Michael <git@mtmc.email>
2015-11-28 18:33:23 -0500

    Adjust navbar to show login if not logged in, logout if logged in

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,25 +4,20 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Laravel Quickstart - Intermediate</title>
-
-    <!-- CSS And JavaScript -->
-    <!-- Latest compiled and minified CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha512-dTfge/zgoMYpP7QbHy4gWMEGsbsdZeCXz7irItjcC3sPUFtf0kuFbDz/ixG7ArTxmDjLXDmezHubeNikyKGVyQ==" crossorigin="anonymous">
-
-    <!-- Optional theme -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-aUGj/X2zp5rLCbBxumKTCw2Z50WgIr1vs/PFN4praOTvYXWlVyh2UtNUU0KAUhAX" crossorigin="anonymous">
-
-    <!-- Latest compiled and minified JavaScript -->
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha512-K1qjQ+NcF2TYO/eI3M6v8EiNYZfA95pQumfvcVrTHtwQVDG+aHRqLi/ETn2uB+1JqwYqVG3LIvdm9lj6imS/pQ==" crossorigin="anonymous"></script>
 </head>
-
 <body>
 <div class="container">
     <nav class="navbar navbar-default">
-        <!-- Navbar Contents --><a href="/auth/logout">Logout</a>
+    @if (Auth::check())
+        <a href="/auth/logout">Logout</a>
+    @else
+        <a href="/auth/logout">Login</a> | <a href="/auth/register">Register</a>
+    @endif
     </nav>
 </div>
-
 @yield('content')
 </body>
 </html>
