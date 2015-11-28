376c0e3b442283997f442a6525c2923637d85dcf
Michael <git@mtmc.email>
2015-11-28 17:07:44 -0500

    Add redirect to /items upon successful login.

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -23,6 +23,8 @@ class AuthController extends Controller
 
     use AuthenticatesAndRegistersUsers, ThrottlesLogins;
 
+    protected $redirectPath = '/items';
+
     /**
      * Create a new authentication controller instance.
      *
